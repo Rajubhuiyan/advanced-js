bf23b33d24063c5b2cc99ab02b6bfb451eafdf05
rajubhuiyan <rajubhuiyaann@gmail.com>
2021-08-13 10:56:33 +0600

    undefined and null clear for interview

---
+++ null-undefined.js
@@ -0,0 +1,19 @@
+let pakhi;
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return;
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premik = {name:"Thomas", phone:39838};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+const ages = [10, 22, 33, 44];
+console.log(ages[4]);
