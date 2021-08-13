4d736ed65671d589753d0c12a3dda52a1f976bba
rajubhuiyan <rajubhuiyaann@gmail.com>
2021-08-13 11:40:17 +0600

    map-filter-done

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,13 @@
+// note:
+// double == hole value check korbe.
+// tripe === hole value + type check korbe
+
+
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("conditons is true");
+}
+else{
+    console.log("conditions is false");
+}
